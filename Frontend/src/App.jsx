d9954388953ee1329f299a0cda6cb5666b3971cc
Gaurav051003 <gauravaryasirohi@gmail.com>
2025-05-09 13:09:43 +0530

    Prepare Vite frontend for deployment

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import Home from "./home/Home";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Courses from "./courses/Courses";
@@ -6,9 +6,21 @@ import Signup from "./components/Signup";
 import { Toaster } from "react-hot-toast";
 import { useAuth } from "./context/AuthProvider";
 
+
 function App() {
   const [authUser, setAuthUser] = useAuth();
   console.log(authUser);
+
+  const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
+
+  useEffect(() => {
+    // ✅ Use it to make an API call
+    fetch(`${apiUrl}/api/books`)
+      .then((res) => res.json())
+      .then((data) => setBooks(data))
+      .catch((err) => console.error('Error fetching books:', err));
+  }, []);
+
   return (
     <>
       <div className="dark:bg-slate-900 dark:text-white">
